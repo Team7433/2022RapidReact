3a574f5ea0c80df2ebee6e289c907549e8b03e71
Ali A <66160329+ashrafyind@users.noreply.github.com>
2022-05-17 20:34:51 +1000

    bottom hoop shooter

---
+++ src/main/cpp/commands/ShooterBottomHoop.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/ShooterBottomHoop.h"
+
+ShooterBottomHoop::ShooterBottomHoop(DualShooter * dualshooter) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements({dualshooter});
+  m_dualshooter = dualshooter;
+}
+
+// Called when the command is initially scheduled.
+void ShooterBottomHoop::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void ShooterBottomHoop::Execute() {
+  m_dualshooter->SetRoller(4000);
+  m_dualshooter->SetShooter(4000);
+}
+
+// Called once the command ends or is interrupted.
+void ShooterBottomHoop::End(bool interrupted) {
+  m_dualshooter->setDualRoller(0.0);
+}
+
+// Returns true when the command should end.
+bool ShooterBottomHoop::IsFinished() {
+  return false;
+}
