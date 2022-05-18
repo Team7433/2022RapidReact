57ef9c622b89a77ef5b66a8d47deaefefc02fdd0
anonymoist <66160329+ashrafyind@users.noreply.github.com>
2022-05-18 16:17:11 +1000

    shooter lambda done + comments

--- src/main/cpp/commands/RunDualShooter.cpp
+++ src/main/cpp/commands/RunDualShooter.cpp
@@ -4,12 +4,12 @@
 
 #include "commands/RunDualShooter.h"
 
-RunDualShooter::RunDualShooter(DualShooter * dualshooter, double rollerTargetVel, double shooterTargetVel, double rampRes) {
+RunDualShooter::RunDualShooter(DualShooter * dualshooter, std::function<double()> rollerTargetVel, std::function<double()> shooterTargetVel, double rampRes) {
   // Use addRequirements() here to declare subsystem dependencies.
   AddRequirements({dualshooter});
   m_dualshooter = dualshooter;
-  m_shooterTargetVel = shooterTargetVel;
-  m_rollerTargetVel = rollerTargetVel;
+  m_shooterTargetVelFunc = shooterTargetVel;
+  m_rollerTargetVelFunc = rollerTargetVel;
   m_rampResolution = rampRes;
 
 
@@ -17,12 +17,14 @@ RunDualShooter::RunDualShooter(DualShooter * dualshooter, double rollerTargetVel
 
 // Called when the command is initially scheduled.
 void RunDualShooter::Initialize() {
+  m_rollerTargetVel = m_rollerTargetVelFunc();
+  m_shooterTargetVel = m_shooterTargetVelFunc();
+
+
   m_shooterDir = fabs(m_shooterTargetVel - m_dualshooter->GetShooterVel()) / (m_shooterTargetVel - m_dualshooter->GetShooterVel());
   m_rollerDir = fabs(m_rollerTargetVel - m_dualshooter->GetRollerVel())/ (m_dualshooter->GetRollerVel() - m_rollerTargetVel);
 
-  currentRollerVel = m_dualshooter->GetRollerVel(); // get the initial roller velocity
-  currentShooterVel = m_dualshooter->GetShooterVel(); // get initial shooter velocity
-
+  
   m_done = false;
   
 }
